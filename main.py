8661b636d8ddd974437f41f72f7432698bcc2f74
Hisham <hisham.khashman@gmail.com>
2026-03-23 14:30:17 +0900

    added post items create endpoint with items.append

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ items = []
 @app.get("/")
 def root():
     return {"hey":"guys"}
+
+# Endpoint to create items by using an http POST
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return item
