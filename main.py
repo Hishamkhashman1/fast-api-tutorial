239900d0bf254ea6025860904bbfc45a6bc9eac5
Hisham <hisham.khashman@gmail.com>
2026-03-23 16:10:31 +0900

    added items list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -14,6 +15,15 @@ def root():
 def create_item(item: str):
     items.append(item)
     return items
+
+# List items endpoint
+
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+
+# Endpoint for getting items by using an http GET with an if for item_id if not found
 
 @app.get("/items/{item_id}")
 def get_item(item_id: int) -> str:
