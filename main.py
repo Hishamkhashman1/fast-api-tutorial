f73e6642a5532a3333b4563b867256dc49c647c1
Hisham <hisham.khashman@gmail.com>
2026-03-23 14:43:13 +0900

    added GET endpoint for items

--- main.py
+++ main.py
@@ -13,4 +13,9 @@ def root():
 @app.post("/items")
 def create_item(item: str):
     items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
     return item
