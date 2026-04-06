e5ca2588206211536440619448f14588a6c3c5d7
Hisham <hisham.khashman@gmail.com>
2026-04-06 10:29:56 +0900

    added Enum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
---
+++ test.py
@@ -0,0 +1,9 @@
+name = "testing Codeium on nvim linux baby hahah "
+
+print(f"hello {name}")
+
+if name == "testing Codeium on nvim linux baby hahah ":
+    print("yes")
+else:
+    print("no")
+
