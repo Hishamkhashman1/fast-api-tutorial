d8842a1e1c607d77f06a5a01ad4beeb413c3162f
Hisham <hisham.khashman@gmail.com>
2026-04-09 23:14:28 +0900

    working on a tutorial like readme

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from enum import Enum
 
+# import fastapi as fastapi
+from fastapi import FastAPI, HTTPException
+
+# define the app as a FastAPI object
 app = FastAPI()
 
-items = []
 
-@app.get("/")
-async def root():
-    return {"hey":"guys"}
+# simple HTTP GET endpoint for a FastAPI app
 
-# Endpoint to create items by using an http POST
+# Register this function as a route handler, it tells FastAPI that when a user makes a GET request to the root URL, the function below will be executed
+@app.get("/") 
 
-@app.post("/items")
-def create_item(item: str):
-    items.append(item)
-    return items
-
-# List items endpoint
-
-@app.get("/items")
-def list_items(limit: int = 10):
-    return items[0:limit]
+#An asynchronous function (often marked with async) allows a program to start a potentially long-running task—such as fetching data, accessing a database, or loading a file—without freezing the entire program. RUNS IN PARALLEL THAT IS.
+async def root(): 
+    # FastAPI automatically converts this python dictionary to JSON format and sends it as the HTTP response
+    return {"hello":"from","fastapi":"world"}
 
 
-# Endpoint for getting items by using an http GET with an if for item_id if not found
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
-    if item_id < len(items):
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail="item not found in list")
